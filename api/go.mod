d58f198e5d0ff5df06f105abb25412206d7cc1a2
nsbuitrago <nsbuitrago@icloud.com>
2022-06-18 15:32:59 -0500

    update basic fcs request and response

--- api/go.mod
+++ api/go.mod
@@ -1,8 +1,12 @@
-module github.com/nsbuitrago/flowfairy/api
+module github.com/nsbuitrago/flowfairy
 
 go 1.17
 
 require (
+	github.com/go-chi/chi v1.5.4
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-chi/cors v1.2.1
+	github.com/go-chi/render v1.0.1
 )
+
+require github.com/nsbuitrago/fcs2 v0.0.0-20220615025118-b30cf9569c37 // indirect
--- api/main.go
+++ api/main.go
@@ -1,36 +1,65 @@
 package main
 
 import (
-	"net/http"
-	"github.com/go-chi/chi/v5"
+	"encoding/json"
+	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
-  "github.com/nsbuitrago/flowfairy/api/models/FlowData"
+	"github.com/go-chi/render"
+	"github.com/nsbuitrago/fcs2/fcs2"
+	"github.com/nsbuitrago/flowfairy/models"
+	"net/http"
 )
 
 func main() {
-  //router setup
-  router := chi.NewRouter()
-  router.Use(middleware.Logger)
-  router.Use(cors.Handler(cors.Options{
-  AllowedOrigins: []string{"http://localhost:3000"},
-  AllowedMethods: []string{"GET", "POST"},
-  }))
+	//router setup
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(render.SetContentType(render.ContentTypeJSON))
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedMethods: []string{"GET", "POST"},
+	}))
 
+	//routes
+	router.Get("/status", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Operational"))
+		if err != nil {
+			return
+		}
+	})
 
-  //routes
-  router.Get("/status", func(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Operational"))
-  })
-
-  router.Post("/load", LoadFlow)
-  http.ListenAndServe(":8000", router)
+	router.Post("/load", LoadFlow)
+	err := http.ListenAndServe(":8000", router)
+	if err != nil {
+		return
+	}
 }
 
 func LoadFlow(w http.ResponseWriter, r *http.Request) {
-  
-  responseObject := &FlowData{}
-  r.ParseMultipartForm(32 >> 20) // 32 mb max memory
+	err := r.ParseMultipartForm(32 >> 20) // 32 mb max memory (rest will be in disk)
+	if err != nil {
+		return
+	}
+
+	_, fileHeader, err := r.FormFile("fcs_file_0")
+	file, err := fileHeader.Open()
+	_, fcsData, err := fcs2.NewDecoder(file).Decode()
+	err = file.Close()
+	if err != nil {
+		return
+	}
+
+	testResponse := models.FlowData{
+		ID:     1,
+		Events: fcsData,
+	}
+
+	//render.Render(w, r, fr)
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(&testResponse)
+	if err != nil {
+		return
+	}
 
 }
-
